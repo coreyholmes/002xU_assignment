c8a3a54422f13fff71e9b5fa66be487e0241ccae
Corey Holmes <coreyholmes20@gmail.com>
2016-04-29 17:08:30 -0700

    iphone spinner on hover

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-/* Enter Javascript below */
+$(function(){
+  $('.iphone, .app-btn').on('mouseover', function(){
+    $('.sun').addClass('zoom');
+  })
+
+  $('.iphone, .app-btn').on('mouseleave', function(){
+    $('.sun').removeClass('zoom');
+  })
+
+});
